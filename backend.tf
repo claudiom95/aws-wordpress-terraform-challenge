8be323f0214f5b0ff284f958cfe20dc104a667c5
claudiom95 <claudiomonteiro95@hotmail.com>
2025-03-30 18:32:24 +0100

    Fix Terraform formatting for backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "wordpress-terraform-state-claudio"
-    key          = "wordpress/terraform.tfstate"
-    region       = "eu-west-3"
-    encrypt      = true
+    bucket  = "wordpress-terraform-state-claudio"
+    key     = "wordpress/terraform.tfstate"
+    region  = "eu-west-3"
+    encrypt = true
   }
 }
