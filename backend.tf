96a8e43f3ffa3dc4a436a76a467ee8c780fabcdd
claudiom95 <claudiomonteiro95@hotmail.com>
2025-03-30 18:30:24 +0100

    Remove unsupported 'use_lockfile' argument from backend config

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket       = "wordpress-terraform-state-claudio"
     key          = "wordpress/terraform.tfstate"
     region       = "eu-west-3"
-    use_lockfile = true
     encrypt      = true
   }
 }
