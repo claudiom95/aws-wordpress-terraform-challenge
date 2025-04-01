8ce796330192af3c3b2c9bdbe3b402e056325ac9
claudiom95 <claudiomonteiro95@hotmail.com>
2025-04-01 23:20:14 +0100

    add route table association to green EC2 instance

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -50,6 +50,11 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.wordpress_vpc.id
 }
