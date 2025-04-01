676c7c290d15270e67f5f257c076d091bf3d8efd
claudiom95 <claudiomonteiro95@hotmail.com>
2025-04-01 21:35:43 +0100

    resolve formatting issues from previous commit

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source                = "./modules/vpc"
   vpc_cidr              = var.vpc_cidr
   public_subnet_cidr    = var.public_subnet_cidr
-  public_subnet_cidr_2 = var.public_subnet_cidr_2
+  public_subnet_cidr_2  = var.public_subnet_cidr_2
   private_subnet_cidr   = var.private_subnet_cidr
   private_subnet_cidr_2 = var.private_subnet_cidr_2
   enable_dns_hostnames  = var.enable_dns_hostnames
