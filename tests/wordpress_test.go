6e728a1fd3dde6d7cbafae198652a9ec8b0a4eb3
claudiom95 <claudiomonteiro95@hotmail.com>
2025-04-03 16:56:53 +0100

    handle quoted ALB DNS output in Terratest for GitHub Actions

--- tests/wordpress_test.go
+++ tests/wordpress_test.go
@@ -33,7 +33,7 @@ func TestWordPressDeployment(t *testing.T) {
 
 	// Get ALB Public DNS output
 	fmt.Println("Getting ALB DNS output")
-	albDNS := terraform.Output(t, terraformOptions, "alb_dns_name")
+	albDNS := terraform.OutputRequired(t, terraformOptions, "alb_dns_name")
 	url := "http://" + albDNS
 	fmt.Printf("ALB URL: %s\n", url)
 
